c141f5c66e4081bdb0f901ea8401108d47613d48
Balaji Pothula <balan.pothula@gmail.com>
2025-08-20 19:22:33 +0530

    updated cURL.sh

--- sh/cURL.sh
+++ sh/cURL.sh
@@ -4,41 +4,12 @@
 # Date        : Wednesday, 23 July 2025,
 # Description : cURL commands.
 
-curl --location 'https://vvjxyyap33.execute-api.eu-central-1.amazonaws.com/'
-
-
-curl -X POST http://localhost:3000/songs \
-  -H "Content-Type: application/json" \
-  -d '{
-    "artist": "Linkin Park",
-    "title": "In the End",
-    "difficulty": 3.7,
-    "level": 5,
-    "released": "2000-10-24T00:00:00Z"
-  }'
-
-curl http://localhost:3000/songs
-
-curl http://localhost:3000/songs/1
-
-curl -X PUT http://localhost:3000/songs/1 \
-  -H "Content-Type: application/json" \
-  -d '{
-    "artist": "Linkin Park",
-    "title": "Numb",
-    "difficulty": 4.0,
-    "level": 6,
-    "released": "2003-03-25T00:00:00Z"
-  }'
-
-curl -X DELETE http://localhost:3000/songs/1
-
-
+curl --location --request GET 'https://go-fiber-app.fly.dev/'
 
 curl \
   --location \
-  --request POST 'https://vvjxyyap33.execute-api.eu-central-1.amazonaws.com/insert/song' \
-  --header 'Content-Type: application/json' \
+  --request POST https://go-fiber-app.fly.dev/put/song \
+  --header "Content-Type: application/json" \
   --data '{
     "artist": "Linkin Park",
     "title": "In the End",
@@ -46,3 +17,21 @@ curl \
     "level": 5,
     "released": "2000-10-24T00:00:00Z"
   }'
+
+curl --location --request GET https://go-fiber-app.fly.dev/get/songs
+
+curl --location --request GET https://go-fiber-app.fly.dev/get/song/1
+
+curl \
+  --location \
+  --request PUT https://go-fiber-app.fly.dev/update/song/1 \
+  --header "Content-Type: application/json" \
+  --data '{
+    "artist": "Linkin Park",
+    "title": "Numb",
+    "difficulty": 4.0,
+    "level": 6,
+    "released": "2003-03-25T00:00:00Z"
+  }'
+
+curl --location --request DELETE https://go-fiber-app.fly.dev/delete/song/1
