e8327077310bf8f4c1e64edea4381c44de50c7ee
Balaji Pothula <balan.pothula@gmail.com>
2025-09-05 20:53:37 +0530

    updated change log

--- postgresql/ahooooy_db/2025-09-05_ChangeLog.sql
+++ postgresql/ahooooy_db/2025-09-05_ChangeLog.sql
@@ -1,6 +1,6 @@
 -- liquibase formatted sql
 
--- changeset BalajiPothula:2025-09-05T15:10:01Z
+-- changeset BalajiPothula:2025-09-05T15:23:01Z
 CREATE TABLE member (
   virtual_number VARCHAR(20) PRIMARY KEY,
   email          VARCHAR(255) UNIQUE NOT NULL,
@@ -14,23 +14,23 @@ CREATE TABLE member (
 );
 --rollback DROP TABLE member;
 
--- changeset BalajiPothula:2025-09-05T15:10:02Z
+-- changeset BalajiPothula:2025-09-05T15:23:02Z
 CREATE INDEX idx_member_email ON member(email);
 --rollback DROP INDEX idx_member_email;
 
--- changeset BalajiPothula:2025-09-05T15:10:03Z
-CREATE OR REPLACE FUNCTION func_updated_at()
+-- changeset BalajiPothula:2025-09-05T15:23:03Z
+CREATE OR REPLACE FUNCTION func_member_updated_at()
 RETURNS TRIGGER AS $$
 BEGIN
   NEW.updated_at = CURRENT_TIMESTAMP;
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
---rollback DROP FUNCTION set_updated_at();
+--rollback DROP FUNCTION func_member_updated_at() CASCADE;
 
--- changeset BalajiPothula:2025-09-05T15:10:04Z
-CREATE TRIGGER trigger_func_updated_at
+-- changeset BalajiPothula:2025-09-05T15:23:04Z
+CREATE TRIGGER trigger_func_member_updated_at
 BEFORE UPDATE ON member
 FOR EACH ROW
 EXECUTE FUNCTION func_updated_at();
---rollback DROP TRIGGER trigger_func_updated_at ON member;
+--rollback DROP TRIGGER trigger_func_member_updated_at ON member;
