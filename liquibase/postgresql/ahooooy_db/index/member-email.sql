090f3e05da2b900b4e816ee38b4d7b51e2fb5441
Balaji Pothula <balan.pothula@gmail.com>
2025-09-06 14:58:18 +0530

    add psql index change mgmt.

--- liquibase/postgresql/ahooooy_db/index/member-email.sql
+++ liquibase/postgresql/ahooooy_db/index/member-email.sql
@@ -1,5 +1,5 @@
 -- liquibase formatted sql
 
--- changeset BalajiPothula:2025-09-05T17:23:48Z
+-- changeset BalajiPothula:2025-09-06T09:21:15Z
 CREATE INDEX idx_member_email ON member(email);
 --rollback DROP INDEX idx_member_email;
