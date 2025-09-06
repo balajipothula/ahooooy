4880b0647bdd6053d7801de88db151368d7a4ed3
Balaji Pothula <balan.pothula@gmail.com>
2025-09-06 18:51:43 +0530

    changeset id updated.

--- liquibase/postgresql/ahooooy_db/function/member-updated_at.sql
+++ liquibase/postgresql/ahooooy_db/function/member-updated_at.sql
@@ -1,6 +1,6 @@
 -- liquibase formatted sql
 
--- changeset BalajiPothula:2025-09-06T09:43:23Z
+-- changeset BalajiPothula:2025-09-06T13:20:37Z
 CREATE OR REPLACE FUNCTION func_member_updated_at()
 RETURNS TRIGGER AS $FUNC$
 BEGIN
@@ -9,4 +9,3 @@ BEGIN
 END;
 $FUNC$ LANGUAGE plpgsql;
 --rollback DROP FUNCTION func_member_updated_at() CASCADE;
-
