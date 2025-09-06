3dd6c8052a2c81bc84ce3d48fd9697777ed2baa3
Balaji Pothula <balan.pothula@gmail.com>
2025-09-06 18:56:32 +0530

    debugging member-updated_at.sql

--- liquibase/postgresql/ahooooy_db/function/member-updated_at.sql
+++ liquibase/postgresql/ahooooy_db/function/member-updated_at.sql
@@ -1,11 +1,11 @@
 -- liquibase formatted sql
 
--- changeset BalajiPothula:2025-09-06T13:20:37Z
+-- changeset BalajiPothula:2025-09-06T13:20:37Z splitStatements:false endDelimiter:$$
 CREATE OR REPLACE FUNCTION func_member_updated_at()
-RETURNS TRIGGER AS $FUNC$
+RETURNS TRIGGER AS $$
 BEGIN
     NEW.updated_at = CURRENT_TIMESTAMP;
     RETURN NEW;
 END;
-$FUNC$ LANGUAGE plpgsql;
+$$ LANGUAGE plpgsql;
 --rollback DROP FUNCTION func_member_updated_at() CASCADE;
